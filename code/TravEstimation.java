3ddec9ac603f3c6a18fa242d8468167f0e79b224
Nefeli Prokopaki Kostopoulou <nefelipk@gmail.com>
2019-04-30 13:31:28 +0300

    Code done. Add images to .tex

--- code/TravEstimation.java
+++ code/TravEstimation.java
@@ -1,24 +1,20 @@
+// In order to run this program you should have installed OpenCV 2.4 (compatible with 
+// Highgui instead of Imgcodecs). Also, there should be four directories containing images
+// exported from a neural network for semantic segmentation. "trainTrav" and "trainObst" 
+// with the traversable and not-traversable images you want to use for training. "evalTrav" 
+// and "evalObst" with the traversable and not-traversable images to use for evaluation.
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileFilter;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-
-import javax.naming.ldap.LdapContext;
-
-import java.util.Properties;
 import java.util.Set;
 
 import org.opencv.core.Core;
@@ -26,20 +22,20 @@ import org.opencv.core.Mat;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
 
+// Use java-8-openjdk library
 import javafx.util.Pair;
 
-public class ReadingImages {
+public class TravEstimation {
 
 	// Path to file that contains classes and colors that correspond to them
-	static final String color_file = "/home/nefeli/workspace/eclipse/roula/src/color150info.txt";
-	// Files representing the folders that contain 1.traversable and 2.non-traversable images
-	static final File train_trav_dir = new File("/home/nefeli/workspace/eclipse/roula/src/trainTrav/");
-	static final File train_obst_dir = new File("/home/nefeli/workspace/eclipse/roula/src/trainObst/");
-//	static final File train_trav_dir = new File("/home/nefeli/workspace/eclipse/roula/src/yes/");
-//	static final File train_obst_dir = new File("/home/nefeli/workspace/eclipse/roula/src/no/");
-	static final File eval_trav_dir = new File("/home/nefeli/workspace/eclipse/roula/src/evalTrav/");
-	static final File eval_obst_dir = new File("/home/nefeli/workspace/eclipse/roula/src/evalObst/");
-	// Map rgb colors to 1.classes, 2.number of traversable and 3. number of non-traversable instances
+	static final String color_file = "<path>/color150info.txt";
+	// Files representing the directories that contain 1-3.traversable and 2-4.non-traversable images 
+	// for 1-2.training and 3-4.evaluation
+	static final File train_trav_dir = new File("<path>/trainTrav/");
+	static final File train_obst_dir = new File("<path>/trainObst/");
+	static final File eval_trav_dir = new File("<path>/evalTrav/");
+	static final File eval_obst_dir = new File("<path>/evalObst/");
+	// Map RGB colors to 1.classes, 2.number of traversable and 3. number of non-traversable instances
 	static Map<String, String> rgbToClass = new HashMap<String, String>();
 	static Map<String, Integer> rgbToTrav = new HashMap<String, Integer>();
 	static Map<String, Integer> rgbToObst = new HashMap<String, Integer>();
@@ -69,6 +65,7 @@ public class ReadingImages {
 	}
 	
 	
+	// Get all RGB colors present in an image
 	static Set<String> getRGBset(File img) {
 		Mat matrix;
 		String rgb;
@@ -96,31 +93,12 @@ public class ReadingImages {
 	
 	
 	static void train(File dir, Map<String, Integer> map) {
-//		Mat matrix;
-//		String rgb;
-//		Set<String> rgb_set = new HashSet<String>();
 		
 		if (dir.isDirectory()) {
 			for (File img : dir.listFiles()) {
 				Set<String> rgb_set = getRGBset(img);
-//				// OpenCV reads image in BGR order
-//				matrix = Highgui.imread(img.getAbsolutePath());
-//				// Convert to RGB order to be compatible with the file containing the classes
-//				Imgproc.cvtColor(matrix, matrix, Imgproc.COLOR_BGR2RGB);
-//				// Keep all colors found in the image
-//				for (int i = 0; i < matrix.rows(); i++) {
-//					for (int j = 0; j < matrix.cols(); j++) {
-//						rgb = "(";
-//						for (int k = 0; k < matrix.channels(); k++) {
-//							rgb += (int) matrix.get(i,j)[k];
-//							if (k != 2) rgb += ", ";
-//							else rgb += ")";
-//						}
-//						rgb_set.add(rgb);
-//					}
-//				}
-//				System.out.println("hash_set: " + rgb_set);
 				
+				// Train
 				// In map: increase by one the instances of each color found in the image
 				for (String temp : rgb_set) {
 					if (map.get(temp) != null) {
@@ -130,75 +108,57 @@ public class ReadingImages {
 						map.put(temp, 1);
 					}
 				}
-//				rgb_set.clear();
 			}
 		}
 	}
 	
 	
 	static Pair<Integer, Integer> eval(File dir) {
-//		Mat matrix;
-//		String rgb;
-//		Set<String> rgb_set = new HashSet<String>();
+		// Count traversable images for two calculating methods
 		int trav_count = 0, trav_count2 = 0;
 		
 		if (dir.isDirectory()) {
 			for (File img : dir.listFiles()) {
 				Set<String> rgb_set = getRGBset(img);
-//				matrix = Highgui.imread(img.getAbsolutePath());
-//				Imgproc.cvtColor(matrix, matrix, Imgproc.COLOR_BGR2RGB);
-//				
-//				for (int i = 0; i < matrix.rows(); i++) {
-//					for (int j = 0; j < matrix.cols(); j++) {
-//						rgb = "(";
-//						for (int k = 0; k < matrix.channels(); k++) {
-//							rgb += (int) matrix.get(i,j)[k];
-//							if (k != 2) rgb += ", ";
-//							else rgb += ")";
-//						}
-//						rgb_set.add(rgb);
-//					}
-//				}
 				
 				// Evaluate
 				double sum_of_trav_perc = 0; 
 				int sum_of_trav = 0, sum_of_obst = 0;
 				Integer trav, obst;
-//				System.out.println("\nNew image!");
+
+				// For each image:
+				// Summarize all RGB colors' traversable instances
+				// Summarize all RGB colors' non-traversable instances
+				// Summarize all RGB colors' traversable percentages
 				for (String temp : rgb_set) {
-//					System.out.println(temp);
 					trav = rgbToTrav.get(temp);
 					obst = rgbToObst.get(temp);
+					
 					if (trav != null && obst != null) {
 						sum_of_trav_perc += (double) trav * 100 / (double) (trav + obst);
 						sum_of_trav += trav;
 						sum_of_obst += obst;
-//						System.out.println("trav != null && obst != null");
 					}
 					else if (trav != null && obst == null) {
 						sum_of_trav_perc += 100;
 						sum_of_trav += trav;
-//						System.out.println("trav != null");
 					}
 					else if (trav == null && obst != null) {
 						sum_of_obst += obst;
-//						System.out.println("obst != null");
 					}
-//					System.out.println("perc: " + df2.format(sum_of_trav_perc) + ", trav: " + sum_of_trav + ", obst: " + sum_of_obst);
 				}
+				// Method 1: Average = traversable percentages of all RGB colors / total of RGB colors 
 				double avg = sum_of_trav_perc / (double) rgb_set.size();
-//				System.out.println("avg: " + df2.format(sum_of_trav_perc) + "/" + rgb_set.size() + " = " + df2.format(avg));
+				// Method 2: Average = traversable instances of all RGB colors / instances of all RGB colors
 				double avg2 = (double) sum_of_trav * 100 / (double) (sum_of_trav + sum_of_obst);
-//				System.out.println("avg2: " + sum_of_trav + " * 100 / (" + sum_of_trav + "+" + sum_of_obst + ") = " + df2.format(avg2));
+
+				// If the average is more than 50% count the image as traversable
 				if (avg >= 50) trav_count++; 
 				if (avg2 >= 50) trav_count2++;
-//				System.out.println("count: " + trav_count);
-//				System.out.println("count2: " + trav_count2);
 			}
 		}
 		
 		return new Pair<Integer, Integer>(trav_count, trav_count2);
-		//return [trav_count, trav_count2];
 	}
 	
 	
@@ -207,9 +167,6 @@ public class ReadingImages {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		
 		readClasses();
-//		for (Entry<String, String> entry : rgbToClass.entrySet()) {
-//		    System.out.println(entry.getKey() + " : " + entry.getValue());
-//		}
 		
 		train(train_trav_dir, rgbToTrav);
 		System.out.println("System trained with traversable images");
@@ -217,6 +174,7 @@ public class ReadingImages {
 		train(train_obst_dir, rgbToObst);
 		System.out.println("System trained with non-traversable images\n");
 		
+		// Print traversability percentages, traversable and obstacle instances of each RGB color (or class label)
 		for (Entry<String, Integer> entry : rgbToTrav.entrySet()) {
 			if (rgbToObst.get(entry.getKey()) != null)
 				System.out.println(entry.getKey() + " : " + rgbToClass.get(entry.getKey())  
